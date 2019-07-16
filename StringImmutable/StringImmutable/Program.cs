b8d297fc3ca791871ebc5ab27e5be1267008d4d1
pbndru <pbndru@gmail.com>
2019-07-16 20:37:25 +0100

    Add files via upload

---
+++ StringImmutable/StringImmutable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringImmutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text, upper;
+            Console.WriteLine("Enter Text: ");
+            text = Console.ReadLine();
+
+            // UNEXPECTED:  Does not convert text to uppercase as its immutable
+            text.ToUpper();            
+            System.Console.WriteLine($"Immutable string {text}", text);
+            
+            // EXPECTED:  convert text to uppercase
+            upper = text.ToUpper();
+            System.Console.WriteLine($"Mutable string {upper}", upper);
+
+            Console.ReadKey();
+        }
+    }
+}
